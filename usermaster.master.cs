fc08a9200d0f5199e0dc1d33ac38e5d1fe11ce06
krinagajera15 <gajerakrina.vnb@gmail.com>
2025-07-23 20:46:07 +0530

    main project commit

--- usermaster.master.cs
+++ usermaster.master.cs
@@ -9,6 +9,46 @@ public partial class usermaster : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //if (!IsPostBack)
+        //{
+        //    if (Session["UserEmail"] != null)
+        //    {
+        //        // Login thay gayu che
+        //        phUserLinks.Visible = true;
+        //        phGuestLinks.Visible = false;
+        //        lblUserName.Text = Session["UserEmail"].ToString(); // athva Session["UserName"]
+        //    }
+        //    else
+        //    {
+        //        // Login nathi thayu
+        //        phUserLinks.Visible = false;
+        //        phGuestLinks.Visible = true;
 
+        //    }
+        //    lblUserName.Text = Session["UserEmail"].ToString();
+        //}
+        if (!IsPostBack)
+        {
+            // OPTIONAL: Show welcome message if user is logged in
+            if (Session["UserEmail"] != null)
+            {
+                lblWelcome.Text = "Welcome, " + Session["UserEmail"].ToString();
+            }
+        }
+    }
+        
+
+    protected void btnLogout_Click(object sender, EventArgs e)
+    {
+        //Session.Clear();             // sessions ખાલી કરો
+        //Session.Abandon();           // completely remove session
+        //Response.Redirect("signin.aspx");  // back to login page
+    }
+
+    protected void lnkLogout_Click(object sender, EventArgs e)
+    {
+        Session.Clear();
+        Session.Abandon();
+        Response.Redirect("signin.aspx");
     }
 }
